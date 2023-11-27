876fb3d2907227631718268550abd75a3bd88d00
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-11-27 19:26:47 +0530

    cewarted AddTodo,and there functionlity

--- reduxtoolkittodo/src/app/store.js
+++ reduxtoolkittodo/src/app/store.js
@@ -1,3 +1,7 @@
 import{configureStore} from '@reduxjs/toolkit';
+import tofoReducer from '.../feature/todo/todoSlice';
+export const store = configureStore({
 
-export const store = configureStore({})
+reducer: todoReducer
+
+})
--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -28,3 +28,5 @@ export const todoSlice =createSlice({
 export const {addTodo, removeTodo} =todoSlice.actions
 
 export default todoSlice.reducer
+
+//150
